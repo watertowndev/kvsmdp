8204d9afaca4b03497808e8795a644fd1877ad24
Maxwell French <maxwell@frenchnet.org>
2024-03-09 19:04:04 -0500

    Add basic command line handling (allow specifying in and out files).

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,50 @@
-mod posthelp;
+use std::error::Error;
+use std::fs::File;
+use std::path::PathBuf;
+use std::io::Write;
+
+use clap::{arg, command, value_parser};
 
 use kvsmdp::datafield::DataFieldDef;
 use kvsmdp::datafile::DataFile;
 
+mod posthelp;
 
-fn main() {
-    println!("Hello, world!");
-    let mut row_defs = vec![];
-    row_defs.push(DataFieldDef::new("AccountNo1", 0, 11, &posthelp::validate_acct));
-    row_defs.push(DataFieldDef::new("CyclNo1", 11, 16, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("Status", 16, 23, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("OwnerName", 23, 49, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("PropAddrKey", 49, 60, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("AddrLine2", 60, 86, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("MeterID", 86, 100, &posthelp::validate_acct));
-    row_defs.push(DataFieldDef::new("CyclNo2", 100, 105, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("ReadDigits", 105, 110, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("No", 110, 114, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("Type", 114, 119, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("ARB", 119, 130, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("FileKey", 130, 141, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("StreetDirection", 141, 151, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("StreetName", 151, 177, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("StreetNumber", 177, 184, &posthelp::trim_zeroes));
-    row_defs.push(DataFieldDef::new("StreetUnit", 184, 191, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("MeterSerial", 191, 205, &posthelp::cleanup));
-    row_defs.push(DataFieldDef::new("PrintKey", 205, 231, &posthelp::fix_printkey));
-    row_defs.push(DataFieldDef::new("MeterSize", 231, 237, &posthelp::fix_meter_size));
-    row_defs.push(DataFieldDef::new("Special", 237, 242, &posthelp::decode_special));
+fn main() -> Result<(), Box<dyn Error>> {
+    let matches = command!()
+        .arg(arg!(<inputfile> "Input file to process").value_parser(value_parser!(PathBuf)))
+        .arg(arg!(<outputfile> "Output file to create").value_parser(value_parser!(PathBuf)))
+        .arg(arg!(<logfile> "Log file to write to").value_parser(value_parser!(PathBuf)))
+        .get_matches();
 
-    let f = DataFile::try_load("c:/temp/test.txt".as_ref(), &row_defs).unwrap();
+    let input_file = matches.get_one::<PathBuf>("inputfile").expect("Input file is required.");
+    let output_file = matches.get_one::<PathBuf>("outputfile").expect("Output file is required.");
+    let log_file = matches.get_one::<PathBuf>("logfile").expect("Log file is required.");
+
+    let row_defs = vec![
+        DataFieldDef::new("AccountNo1", 0, 11, &posthelp::validate_acct),
+        DataFieldDef::new("CyclNo1", 11, 16, &posthelp::cleanup),
+        DataFieldDef::new("Status", 16, 23, &posthelp::cleanup),
+        DataFieldDef::new("OwnerName", 23, 49, &posthelp::cleanup),
+        DataFieldDef::new("PropAddrKey", 49, 60, &posthelp::cleanup),
+        DataFieldDef::new("AddrLine2", 60, 86, &posthelp::cleanup),
+        DataFieldDef::new("MeterID", 86, 100, &posthelp::validate_acct),
+        DataFieldDef::new("CyclNo2", 100, 105, &posthelp::cleanup),
+        DataFieldDef::new("ReadDigits", 105, 110, &posthelp::cleanup),
+        DataFieldDef::new("No", 110, 114, &posthelp::cleanup),
+        DataFieldDef::new("Type", 114, 119, &posthelp::cleanup),
+        DataFieldDef::new("ARB", 119, 130, &posthelp::cleanup),
+        DataFieldDef::new("FileKey", 130, 141, &posthelp::cleanup),
+        DataFieldDef::new("StreetDirection", 141, 151, &posthelp::cleanup),
+        DataFieldDef::new("StreetName", 151, 177, &posthelp::cleanup),
+        DataFieldDef::new("StreetNumber", 177, 184, &posthelp::trim_zeroes),
+        DataFieldDef::new("StreetUnit", 184, 191, &posthelp::cleanup),
+        DataFieldDef::new("MeterSerial", 191, 205, &posthelp::cleanup),
+        DataFieldDef::new("PrintKey", 205, 231, &posthelp::fix_printkey),
+        DataFieldDef::new("MeterSize", 231, 237, &posthelp::fix_meter_size),
+        DataFieldDef::new("Special", 237, 242, &posthelp::decode_special),
+    ];
+    let f = DataFile::try_load(input_file, &row_defs).unwrap();
 
     println!("{} entries, {} warnings", f.rows().len(), f.warnings().len());
     let output_fields = vec![
@@ -63,6 +78,14 @@ fn main() {
         csv_rows.push(csvrow);
     }
 
-    println!("{:?}", csv_rows);
+    println!("Found {} rows with {} warnings along the way, resulting in {} CSV rows.",
+             f.rows().len(), f.warnings().len(), csv_rows.len());
+
+    let outfile = File::create(output_file)?;
+    for row in csv_rows {
+        writeln!(&outfile, "{}", row)?;
+    }
+
+    Ok(())
 }
 
